795f79b87fd998602db53d5decb36737e3249e1e
wonecode <lochlainn.gadault@gmail.com>
2026-06-10 17:36:42 +0200

    feat: changelog doc page

--- .source/browser.ts
+++ .source/browser.ts
@@ -7,6 +7,6 @@ const create = browser<typeof Config, import("fumadocs-mdx/runtime/types").Inter
   }
 }>();
 const browserCollections = {
-  docs: create.doc("docs", {"commands.mdx": () => import("../content/docs/commands.mdx?collection=docs"), "faq.mdx": () => import("../content/docs/faq.mdx?collection=docs"), "getting-started.mdx": () => import("../content/docs/getting-started.mdx?collection=docs"), "index.mdx": () => import("../content/docs/index.mdx?collection=docs"), "scoring.mdx": () => import("../content/docs/scoring.mdx?collection=docs"), }),
+  docs: create.doc("docs", {"changelog.mdx": () => import("../content/docs/changelog.mdx?collection=docs"), "commands.mdx": () => import("../content/docs/commands.mdx?collection=docs"), "faq.mdx": () => import("../content/docs/faq.mdx?collection=docs"), "getting-started.mdx": () => import("../content/docs/getting-started.mdx?collection=docs"), "index.mdx": () => import("../content/docs/index.mdx?collection=docs"), "scoring.mdx": () => import("../content/docs/scoring.mdx?collection=docs"), }),
 };
 export default browserCollections;
--- .source/server.ts
+++ .source/server.ts
@@ -1,9 +1,10 @@
 // @ts-nocheck
-import * as __fd_glob_4 from "../content/docs/scoring.mdx?collection=docs"
-import * as __fd_glob_3 from "../content/docs/index.mdx?collection=docs"
-import * as __fd_glob_2 from "../content/docs/getting-started.mdx?collection=docs"
-import * as __fd_glob_1 from "../content/docs/faq.mdx?collection=docs"
-import * as __fd_glob_0 from "../content/docs/commands.mdx?collection=docs"
+import * as __fd_glob_5 from "../content/docs/scoring.mdx?collection=docs"
+import * as __fd_glob_4 from "../content/docs/index.mdx?collection=docs"
+import * as __fd_glob_3 from "../content/docs/getting-started.mdx?collection=docs"
+import * as __fd_glob_2 from "../content/docs/faq.mdx?collection=docs"
+import * as __fd_glob_1 from "../content/docs/commands.mdx?collection=docs"
+import * as __fd_glob_0 from "../content/docs/changelog.mdx?collection=docs"
 import { server } from 'fumadocs-mdx/runtime/server';
 import type * as Config from '../source.config';
 
@@ -12,4 +13,4 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const docs = await create.docs("docs", "content/docs", {}, {"commands.mdx": __fd_glob_0, "faq.mdx": __fd_glob_1, "getting-started.mdx": __fd_glob_2, "index.mdx": __fd_glob_3, "scoring.mdx": __fd_glob_4, });
+export const docs = await create.docs("docs", "content/docs", {}, {"changelog.mdx": __fd_glob_0, "commands.mdx": __fd_glob_1, "faq.mdx": __fd_glob_2, "getting-started.mdx": __fd_glob_3, "index.mdx": __fd_glob_4, "scoring.mdx": __fd_glob_5, });
