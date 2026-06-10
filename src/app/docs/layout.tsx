3f40d260fe3541aaf2caf44bb1a56361a43cf45e
wonecode <lochlainn.gadault@gmail.com>
2026-06-10 20:08:27 +0200

    feat: some changes before release

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,7 +1,7 @@
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import type { ReactNode } from 'react';
 import { source } from '@/lib/source';
-import {Zap} from "lucide-react";
+import { Zap } from 'lucide-react';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
@@ -10,7 +10,7 @@ export default function Layout({ children }: { children: ReactNode }) {
       nav={{
         title: (
           <span className="flex items-center gap-2 font-semibold">
-            <img alt='banana' src="android-chrome-192x192.png" className='w-5 h-5' />
+            <img alt="banana" src="android-chrome-192x192.png" className="h-5 w-5" />
             <span>Bananalyz</span>
           </span>
         ),
@@ -27,20 +27,28 @@ export default function Layout({ children }: { children: ReactNode }) {
           url: 'https://x.com/wonezer',
           external: true,
         },
-          {
-              type: 'icon',
-              label: 'Follow me on X',
-              icon: <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="currentColor">
-                  <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-4.714-6.231-5.401 6.231H2.744l7.737-8.836L1.254 2.25H8.08l4.253 5.622L18.244 2.25zm-1.161 17.52h1.833L7.084 4.126H5.117L17.083 19.77z" />
-              </svg>,
-              text: 'X',
-              url: 'https://x.com/wonezer',
-          },
+        {
+          type: 'icon',
+          label: 'Follow me on X',
+          icon: (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="15"
+              height="15"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-4.714-6.231-5.401 6.231H2.744l7.737-8.836L1.254 2.25H8.08l4.253 5.622L18.244 2.25zm-1.161 17.52h1.833L7.084 4.126H5.117L17.083 19.77z" />
+            </svg>
+          ),
+          text: 'X',
+          url: 'https://x.com/wonezer',
+        },
       ]}
       sidebar={{
         footer: (
-          <div className="flex items-center gap-1 mt-4 text-xs opacity-30">
-              <Zap  size={12}/>
+          <div className="mt-4 flex items-center gap-1 text-xs opacity-30">
+            <Zap size={12} />
             Powered by Jungle Squad
           </div>
         ),
